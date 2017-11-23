4ed43112f5047eb05020df539f31ef69a386e374
Mikhail Zolotukhin <Zomial@yandex.ru>
2017-11-23 21:12:24 +0300

    show and <<

--- src/Graph.h
+++ src/Graph.h
@@ -1,5 +1,5 @@
 /*
- * Declares graph
+ * Simple oriented graph, implemented as edges list
  * Copyright (C) 2017  Mikhail <zomial@yandex.ru>
  * 
  * This program is free software; you can redistribute it and/or modify
@@ -20,8 +20,45 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
+
+#include <utility>
+#include <iostream>
+#include "Set.h"
+
 template <typename T>
 class Graph {
+private:
+    std::list < std::pair<T,T> > edges;
+
+public:
+    Graph() : edges{} {};
+    Graph(const Graph&) = delete;
+    Graph(Graph&&) = delete;
+    Graph operator = (const Graph&) = delete;
+    Graph operator = (Graph&&) = delete;
+    ~Graph() {};
+
+    void add_edge(std::pair<T,T> edge) {
+        edges.push_back(edge);
+    };
+
+    void remove_edge(std::pair<T,T> edge) {
+        edges.remove(edge);
+    };
+
+    void add_edge_from_to(T from, T to) {
+        add_edge(std::pair<T,T>{from, to});
+    }
+
+    void show() {
+        std::cout << *this << std::endl;
+    }
+
+    friend std::ostream& operator << ( std::ostream& os, const Graph& graph) {
+        for (auto elem : graph.edges)
+            os << "{ " << elem.first << ", " << elem.second << " }; \n";
+        return os;
+    };
 
 };
 
