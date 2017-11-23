4c0f7e342df0235c031b425a3e676be3fed56961
Mikhail Zolotukhin <Zomial@yandex.ru>
2017-11-23 21:11:23 +0300

    Basic checking

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
+#include <string>
+#include <vector>
 #include "Transversal.h"
+#include "Set.h"
 
 int main () {
     using namespace std;
 
-    cout << "Hello graphs!" << endl;
+    std::vector< Set<string> > sets{
+        Set<string>{"145"},
+        Set<string>{"1"},
+        Set<string>{"234"},
+        Set<string>{"24"}
+    };
+
+    Transversal<string> tr {sets};
+
+    for (auto set : sets) {
+        cout << set << endl;
+    }
+
+    tr.show_common_set();
+    tr.show_graph();
 
     return 0;
 }
