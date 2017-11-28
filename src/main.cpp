ded89301f113f9d348bec4353c470c332ea359af
Mikhail Zolotukhin <Zomial@yandex.ru>
2017-11-28 22:36:46 +0300

    Code formatting, C++ style generatinos using strings, cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
-#include "stdafx.h"
 #include <iostream>
 #include <string>
 #include <vector>
 #include <time.h>
+#include <stdexcept>
 #include "Transversal.h"
 #include "Set.h"
 
@@ -13,89 +13,82 @@ vector< Set<string> > sets_input();
 
 int main () {
 
-	vector< Set<string> > sets{
-		Set<string>{"145"},
-		Set<string>{"1"},
-		Set<string>{"234"},
-		Set<string>{"24"}
-		};
-	char c;
-	cout << "Choose sets generation or keyboard input (G/K)" << endl;
-	char choose;
-	cin >> choose;
-	switch (choose)
-	{
-		case 'g':
-		{
-			vector< Set<string> > sets_gen = sets_generation();
-			sets = sets_gen;
-			break;
-		}
-		case 'k':
-		{
-			vector< Set<string> > sets_key = sets_input();
-			sets = sets_key;
-			break;
-		}
-		default:
-		{
-			cout << "No one option choosed, program will use custom sets" << endl;
-			break;
-		}
-	}
-	
-   Transversal<string> tr {sets};
+    cout << "Input sets:" << endl;
+    cout << "Choose input method: random generated (R) or input custom ones? (C): ";
+    char choice {0};
+    cin >> choice;
 
-    for (auto set : sets) {
-        cout << set << endl;
+    vector< Set<string> > sets{};
+    switch (choice)
+    {
+        case 'r':
+        case 'R':
+            sets = sets_generation();
+        break;
+
+        case 'c':
+        case 'C':
+            sets = sets_input();
+        break;
+
+        default:
+            cerr << "You haven't choose input method!" << endl;
+            throw runtime_error{"Wrong option"};
+            break;
     }
 
+    Transversal<string> tr {sets};
+
+    for (size_t i = 0; i != sets.size(); ++i) {
+        cout << "Set " << i+1 << ": " << sets[i] << endl;
+    }
+
+    cout << "Common set: " << endl;
     tr.show_common_set();
+
+    cout << "Graph: " << endl;
     tr.show_graph();
-	system("pause");
+
     return 0;
 }
 
 vector< Set<string> > sets_generation()
 {
-	srand(time(NULL));
-	int num_sets = rand() % 10;
-	char mask[] = "123456789";
-	char ch_to_string[10];
-	std::vector< Set<string> > sets(num_sets);
-	int i, j, k;
-	string str;
-	for (i = 0; i < num_sets; i++)
-	{
-		k = 0;
-		for (j = 0; j < 10; j++)
-		{
-			if (rand() % 2 == 1)
-			{
-				ch_to_string[k] = mask[j];
-				k++;
-			}
-		}
-		ch_to_string[k] = '\0';
-		str = ch_to_string;
-		sets[i] = str;
-	}
+    srand(time(NULL));
+    enum {MAX_NUMBER_OF_SETS = 10};
 
-	return sets;
+    std::vector< Set<string> > sets{};
+    int number_of_sets = rand() % MAX_NUMBER_OF_SETS + 1;
+
+    static const string mask {"0123456789"};
+
+    for (int i = 0; i < number_of_sets; i++)
+    {
+        string str_to_add{};
+        for (auto ch : mask)
+        {
+            if (rand() % 2)
+                str_to_add += ch;
+        }
+        sets.push_back(str_to_add);
+    }
+
+    return sets;
 }
 
 vector< Set<string> > sets_input()
 {
-	cout << "Choose number of sets" << endl;
-	int num;
-	cin >> num;
-	std::vector< Set<string> > sets(num);
-	string str;
-	for (int i = 0; i < num; i++)
-	{
-		cout << "Input set " << i + 1 << ": ";
-		cin >> str;
-		sets[i] = str;
-	}
-	return sets;
+    cout << "Choose number of sets: " << endl;
+    size_t num {0};
+    cin >> num;
+
+    std::vector< Set<string> > sets{};
+    for (int i = 0; i < num; i++)
+    {
+        string str{};
+        cout << "Input set " << i + 1 << ": ";
+        cin >> str;
+        sets.push_back(str);
+    }
+    return sets;
 }
